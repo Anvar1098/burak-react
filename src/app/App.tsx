f827e1a60a3c4fd5a60df2eeb0cfb956f0b5a3ea
Anvar1098 <anvar00aminov@gmail.com>
2025-01-12 01:19:49 +0900

    feat:HomeNavbar building

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "../css/app.css";
 import { Box, Button, Container, Stack, Typography } from "@mui/material";
-import { RippleBadge } from "./MaterialTheme/styled";
+// import { RippleBadge } from "./MaterialTheme/styled";
 import { Link, Route, Switch, useLocation } from "react-router-dom";
 import { HomePage } from "./screens/homePage";
 import { ProductsPage } from "./screens/productsPage";
@@ -10,6 +10,8 @@ import { UserPage } from "./screens/userPage";
 import { HomeNavbar } from "./components/headers/HomeNavbar";
 import { OtherNavbar } from "./components/headers/OtherNavbar";
 import { Footer } from "./components/footer";
+import "../css/app.css";
+import "../css/navbar.css";
 
 function App() {
   const location = useLocation();
--- src/app/components/headers/HomeNavbar.tsx
+++ src/app/components/headers/HomeNavbar.tsx
@@ -1,4 +1,32 @@
-export function HomeNavbar() {
-    return <div>HomeNavbar</div>;
-}
+import { Box, Container, Stack } from "@mui/material";
+import { NavLink } from "react-router-dom";
 
+export function HomeNavbar() {
+  return (
+    <div className="home-navbar">
+      <Container sx={{ mt: "55px", height: "642px" }}>
+        <Stack
+          sx={{ height: "50px" }}
+          flexDirection={"row"}
+          justifyContent={"space-between"}
+          alignItems={"center"}
+        >
+          <Box>
+            <NavLink to="/">
+              <img style={{ width: "125px", height: "30px" }} />
+            </NavLink>
+          </Box>
+          <Stack
+            flexDirection={"row"}
+            justifyContent={"space-between"}
+            minWidth={"700px"}
+            alignItems={"center"}
+          >
+            2
+          </Stack>
+          <Stack>DETAIL</Stack>
+        </Stack>
+      </Container>
+    </div>
+  );
+}

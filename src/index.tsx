0fb9ffd0ab562a79ba6bb8b2227203f2985367b9
Anvar1098 <anvar00aminov@gmail.com>
2025-01-14 19:11:32 +0900

    fix: develop popular dishes sectional component

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { createRoot } from "react-dom/client"
-import { Provider } from "react-redux";
+import { Provider } from "react-redux";  // malumotlar bunkeri
 import { store } from "./app/store";
 import App from "./app/App";
 import reportWebVitals from "./reportWebVitals";
-import CssBaseline from "@mui/material/CssBaseline";
-import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";  // 
+import { ThemeProvider } from "@mui/material/styles"; // 
 import theme from "./app/MaterialTheme";
 import "./css/index.css";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";

4aa9e2fd919baf7f3935ee8c7d76380169f1b0ea
Anvar1098 <anvar00aminov@gmail.com>
2025-01-15 18:45:33 +0900

    feat:Events component

--- src/app/screens/homePage/ActiveUsers.tsx
+++ src/app/screens/homePage/ActiveUsers.tsx
@@ -1,11 +1,48 @@
-import React from "react";
-import { Container } from "@mui/material";
+import { Box, Container, Stack } from "@mui/material";
+import Card from "@mui/joy/Card";
+import { CardContent, CssVarsProvider, Typography } from "@mui/joy";
+import CardOverflow from "@mui/joy/CardOverflow";
+import AspectRatio from "@mui/joy/AspectRatio";
+
+const activeUsers = [
+  { memberNick: "Jack", memberImage: "/img/Jack.jpg" },
+  { memberNick: "Justin", memberImage: "/img/justin.webp" },
+  { memberNick: "Rose", memberImage: "/img/rose.webp" },
+  { memberNick: "Nusret", memberImage: "/img/nusret.webp" },
+];
 
 export default function ActiveUsers() {
-    return (
-        <div>
-            <Container>ActiveUsers</Container>
-        </div>
-    )
+  return (
+    <div className="active-users-frame">
+      <Container>
+        <Stack className="main">
+          <Box className="category-title">Active Users</Box>
+          <Stack className="cards-frame">
+            <CssVarsProvider>
+              {activeUsers.length !== 0 ? (
+                activeUsers.map((ele, index) => (
+                  <Card className="card" key={index}>
+                    <CardOverflow>
+                      <AspectRatio ratio="1">
+                        <img src={ele.memberImage} alt="" />
+                      </AspectRatio>
+                    </CardOverflow>
+                    <Stack className="member-nickname">
+                      <CardContent>
+                        <Typography>
+                          {ele.memberNick}
+                        </Typography>
+                      </CardContent>
+                    </Stack>
+                  </Card>
+                ))
+              ) : (
+                <Box className="no-data">No active users available!</Box>
+              )}
+            </CssVarsProvider>
+          </Stack>
+        </Stack>
+      </Container>
+    </div>
+  );
 }
-   
